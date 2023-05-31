b77193d2f53bbf2c29034fc1f9a6a4ff4ac36958
Averrous Saloom <averous009@gmail.com>
2023-05-31 23:02:58 +0700

    initialization

---
+++ src/test/java/com/stream/streambackend/StreamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stream.streambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
